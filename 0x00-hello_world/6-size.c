5f56663d38d78dc4a88d2e1c6f24e9c8b48cdc3d
Cephei14 <rafik.dhaibi@gmail.com>
2023-08-10 01:00:16 -0700

    i hate betty

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %ls byte(s)", sizeof(char));
+	printf("Size of a int: %ls byte(s)", sizeof(int));
+	printf("Size of a long int: %ls byte(s)", sizeof(long in));
+	printf("Size of a long long: %ls byte(s)", sizeof(long long));
+	printf("Size of a float: %ls byte(s)", sizeof(float));
+	return (0);
+}
