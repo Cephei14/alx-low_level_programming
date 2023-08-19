8f715a934a21fdc686af0e2c5f3b153e6af395ca
Cephei14 <rafik.dhaibi@gmail.com>
2023-08-19 09:53:21 -0700

    long msg

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - main function
+ * Return: 1
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 53);
+	return (1);
+}
