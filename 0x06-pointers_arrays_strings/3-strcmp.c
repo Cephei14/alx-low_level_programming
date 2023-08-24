450f502922516c6439513f124925e50b4ddf0148
Cephei14 <rafik.dhaibi@gmail.com>
2023-08-23 19:35:05 -0700

    pushing strcmp task that compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_strcmp - function that compare 2 strings
+ *@s1: first string
+ *@s2: second string
+ *Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int c;
+
+	c = strcmp(s1, s2);
+	return (c);
+}
