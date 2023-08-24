8ba8de1b7c193fa532ff94a5e825f824ce711018
Cephei14 <rafik.dhaibi@gmail.com>
2023-08-23 18:43:06 -0700

    pushing strncpy task

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_strncpy - function that copy a string
+ *@dest: copy destination
+ *@src: what to copy
+ *@n: limiting the bytes of src to be transported
+ *Return: 0
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *c;
+
+	c = strncpy(dest, src, n);
+	return (c);
+}
