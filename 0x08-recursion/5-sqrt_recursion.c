c35f80ea08b953621752e245d16d785baaf66233
Cephei14 <rafik.dhaibi@gmail.com>
2023-08-30 19:29:04 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - square root of number function
+ * _sqrt_recursive - approximative function for square
+ * _sqrt_recursion - function that calculate sqrt
+ * @s: parameter
  * @n: number in question
- * @g:approximation to squr
+ * @e:parameter
  * Return: square root or -1
  */
 
 int _sqrt_recursive(int n, int s, int e)
 {
 	int m, sq;
-       
+
 	m = s + (e - s) / 2;
 	sq = m * m;
 	if (s > e)
@@ -30,6 +32,13 @@ int _sqrt_recursive(int n, int s, int e)
 	return (_sqrt_recursive(n, s, m - 1));
 	}
 }
+
+/**
+ ** _sqrt_recursion - square root of number function
+ *@n: parameter
+ *Return: -1 if error or sqrt of n
+ */
+
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
