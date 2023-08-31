36ce5cb8fd9ed441987b330e7ef62d79253737a7
Cephei14 <rafik.dhaibi@gmail.com>
2023-08-30 19:18:56 -0700

    sqrt rec

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,26 +1,40 @@
 #include "main.h"
-#include <math.h>
+
 /**
  * _sqrt_recursion - square root of number function
  * @n: number in question
+ * @g:approximation to squr
  * Return: square root or -1
  */
 
+int _sqrt_recursive(int n, int s, int e)
+{
+	int m, sq;
+       
+	m = s + (e - s) / 2;
+	sq = m * m;
+	if (s > e)
+	{
+		return (-1);
+	}
+	if (sq == n)
+	{
+		return (m);
+	}
+	else if (sq < n)
+	{
+		return (_sqrt_recursive(n, m + 1, e));
+	}
+	else
+	{
+	return (_sqrt_recursive(n, s, m - 1));
+	}
+}
 int _sqrt_recursion(int n)
 {
-	double res;
-
-	res = sqrt(n);
 	if (n < 0)
 	{
 		return (-1);
 	}
-	if (res - (int)res == 0)
-	{
-		return (res);
-	}
-	else
-	{
-		return (-1);
-	}
+	return (_sqrt_recursive(n, 0, n));
 }
