4b15ce2d3df9444dc8bc9b79d9527fe8d0f63d95
Cephei14 <rafik.dhaibi@gmail.com>
2023-09-03 22:17:16 -0700

    my name task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - main function
+ * @argc: number of command line
+ * @argv: array of strings
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
