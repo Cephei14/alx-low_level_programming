a76889d9541a5d703fb3aeec44f8d98134fc2abc
Cephei14 <rafik.dhaibi@gmail.com>
2023-09-28 19:05:36 -0700

    optimus prime task

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int check(int n, int i);
+
 /**
  * is_prime_number - function that check if number is prime or not
  * @n: the number in question
@@ -8,5 +10,26 @@
 
 int is_prime_number(int n)
 {
+	if (n <= 1)
+		return (0);
+	return (check(n, n - 1));
+}
 
+/**
+ * check - function that check if n is primal
+ * @n: number in question
+ * @i: number to increment
+ * Return: result if n is prime
+ */
+
+int check(int n, int i)
+{
+	int k = 0;
+
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	k = check(n, i - 1);
+	return (k);
 }
