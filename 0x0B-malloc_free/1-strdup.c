f2d8afd207281497cf47b55b40d6e6b1917ccf14
Cephei14 <rafik.dhaibi@gmail.com>
2023-10-04 03:26:45 -0700

    malloc free task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strdup - function that return the address to an allocated memory
+ * @str: a tring
+ * Return: a pointer
+ */
+
+char *_strdup(char *str)
+{
+	long unsigned int i, c;
+	char *p;
+
+	for (c = 0; str[c] <= '\0'; c++)
+		c++;
+	p = malloc(c);
+	if (str == NULL || p == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; str[i]; i++)
+		{
+			p[i] = str[i];
+		}
+	}
+	return(p);
+}
