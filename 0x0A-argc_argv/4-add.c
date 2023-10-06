46877ce9a41bd976a7e973fba820f85a9ab77a3a
Cephei14 <rafik.dhaibi@gmail.com>
2023-10-06 06:11:50 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * main - the main function
+ * @argc: number or agruments
+ * @argv: name of arguments
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int x, y, add = 0;
+
+	for (x = 1; x < argc; x++)
+	{
+		for (y = 0; argv[x][y] != '\0'; y++)
+		{
+			if (!isdigit(argv[x][y]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[x]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
