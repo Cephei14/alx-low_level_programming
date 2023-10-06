5249fac4421d28c1af7fa876eed48959f645d93f
Cephei14 <rafik.dhaibi@gmail.com>
2023-10-05 17:48:16 -0700

    malloc free task grid #3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * alloc_grid - function that create 2D array of int
+ * @width: width of the grid
+ * @height: the height of the grid
+ * Return: a pointer
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j, **p;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	p = (int **)malloc(sizeof(int) * height);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		p[i] = (int *)malloc(sizeof(int) * width);
+		if (p[i] == NULL)
+			return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			p[i][j] = 0;
+	}
+	return (p);
+}
