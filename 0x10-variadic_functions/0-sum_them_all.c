51f3a1c0c47228a80c4ca4e674fa9d07a9a8de28
Cephei14 <rafik.dhaibi@gmail.com>
2023-10-12 05:57:51 -0700

    check 5

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,22 +2,23 @@
 
 /**
  * sum_them_all - function that perform addition
- * @n: number of args
+ * @n: number of arguments
  * Return: 0 or sum
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list args;
 	unsigned int i;
 	int s;
-	va_list args;
 
 	if (n == 0)
 		return (0);
-	s = 0;
 	va_start(args, n);
+	s = 0;
 	for (i = 0; i < n; i++)
 		s += va_arg(args, int);
 	va_end(args);
 	return (s);
 }
+
