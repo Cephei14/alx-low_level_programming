cd02cf95559fe10919e3ab899fda16d36831a75c
Cephei14 <rafik.dhaibi@gmail.com>
2023-10-13 00:19:45 -0700

    var lst task

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,14 +5,42 @@
  * @format: list of args types
  * Return: nothing
  */
-
 void print_all(const char * const format, ...)
 {
 	va_list PA;
+	int i;
+	char *c, *sp;
 
+	i = 0;
+	sp = ", ";
 	va_start(PA, format);
-	printf("%c ,", va_arg(PA, int));
-	printf("%d ,", va_arg(PA, int));
-	printf("%s\n", va_arg(PA, char *));
-	va_end(PA);
+	while (format[i] != '\0')
+	{
+		if (format[i + 1] == '\0')
+			sp = "";
+		switch (format[i])
+		{
+			case'c':
+				printf("%c%s", va_arg(PA, int), sp);
+			break;
+			case'i':
+				printf("%d%s", va_arg(PA, int), sp);
+			break;
+			case'f':
+				printf("%f%s", va_arg(PA, double), sp);
+			break;
+			case's':
+			{
+				c = va_arg(PA, char*);
+				if (c == NULL)
+					c = "(nil)";
+				printf("%s%s", c, sp);
+			}
+			break;
+			default:
+			break;
+		}
+		i++;
+	}
+	printf("\n");
 }
