55b4aec2020fc5410ff1417e542ca89730d906c8
Cephei14 <rafik.dhaibi@gmail.com>
2023-10-12 20:46:33 -0700

    test

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,4 +1,18 @@
-#include "vardiac_functions.h"
+#include "variadic_functions.h"
 
 /**
  * print_all - funtion that print anything
+ * @format: list of args types
+ * Return: nothing
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list PA;
+
+	va_start(PA, format);
+	printf("%c ,", va_arg(PA, int));
+	printf("%d ,", va_arg(PA, int));
+	printf("%s\n", va_arg(PA, char *));
+	va_end(PA);
+}
