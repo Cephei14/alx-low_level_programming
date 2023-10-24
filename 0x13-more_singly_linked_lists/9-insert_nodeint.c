a1233c109ee9f69a7c7ebf1608b1b2d6d344dae4
Cephei14 <rafik.dhaibi@gmail.com>
2023-10-24 04:52:56 -0700

    task 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,67 @@
+#include "lists.h"
+size_t listint_len(const listint_t *h);
+
+/**
+ * insert_nodeint_at_index - function that insert a node in idx position
+ * @head: the head of the linked list
+ * @idx: the position desired to insert a new node
+ * @n: data of the node
+ * Return: a pointer to the new created node
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *p, *ptr;
+	unsigned int c, i = 0;
+
+	c = listint_len(*head);
+	if (*head == NULL || idx > c)
+		return (NULL);
+	p = malloc(sizeof(listint_t));
+	if (p == NULL)
+		return (NULL);
+	p->n = n;
+	ptr = *head;
+	if (idx == 0)
+	{
+		p->next = *head;
+		*head = p;
+		return (p);
+	}
+	if (idx == c)
+	{
+		while (ptr->next != NULL)
+		{
+			ptr = ptr->next;
+		}
+		p->next = NULL;
+		ptr->next = p;
+		return (p);
+	}
+	while (i < idx - 1)
+	{
+		ptr = ptr->next;
+		i++;
+	}
+	p->next = ptr->next;
+	ptr->next = p;
+	return (p);
+}
+
+/**
+ * listint_len - function that return number of elements in a linked list
+ * @h: head of the listed list
+ * Return: the number of elements in the linked list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	unsigned int i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
