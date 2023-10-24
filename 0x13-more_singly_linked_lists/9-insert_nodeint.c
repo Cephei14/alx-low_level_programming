f69ffc41116fa1c9ecbcc5284d555885d2ac6812
Cephei14 <rafik.dhaibi@gmail.com>
2023-10-24 05:54:54 -0700

    last check

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,33 +12,22 @@ size_t listint_len(const listint_t *h);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *p, *ptr;
-	unsigned int c, i = 0;
+	unsigned int i = 0;
 
-	c = listint_len(*head);
-	if (*head == NULL || idx > c)
+	if (*head == NULL && idx != 0)
 		return (NULL);
 	p = malloc(sizeof(listint_t));
 	if (p == NULL)
 		return (NULL);
 	p->n = n;
 	ptr = *head;
-	if (idx == 0)
+	if (idx == 0 || ptr == NULL)
 	{
 		p->next = *head;
 		*head = p;
 		return (p);
 	}
-	if (idx == c - 1)
-	{
-		while (ptr->next != NULL)
-		{
-			ptr = ptr->next;
-		}
-		p->next = NULL;
-		ptr->next = p;
-		return (p);
-	}
-	while (i < idx - 1)
+	while (i < idx - 1 && ptr->next != NULL)
 	{
 		ptr = ptr->next;
 		i++;
@@ -46,22 +35,4 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	p->next = ptr->next;
 	ptr->next = p;
 	return (p);
-}
-
-/**
- * listint_len - function that return number of elements in a linked list
- * @h: head of the listed list
- * Return: the number of elements in the linked list
- */
-
-size_t listint_len(const listint_t *h)
-{
-	unsigned int i = 0;
-
-	while (h)
-	{
-		h = h->next;
-		i++;
-	}
-	return (i);
 }
