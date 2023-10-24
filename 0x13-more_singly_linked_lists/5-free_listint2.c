4be6c4856e38255e35073c5292620e6967a35aa5
Cephei14 <rafik.dhaibi@gmail.com>
2023-10-23 18:55:02 -0700

    free head NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that free everything
+ * @head: head of the linked list
+ * Return: nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *p;
+
+	while (*head)
+	{
+		p = *head;
+		*head = (*head)->next;
+		free(p);
+	}
+	*head = NULL;
+}
