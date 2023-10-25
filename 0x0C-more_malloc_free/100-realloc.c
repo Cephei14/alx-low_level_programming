012fa09fd6f02524ca134232a34d3dd430b473eb
Cephei14 <rafik.dhaibi@gmail.com>
2023-10-25 06:01:12 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,17 +13,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *p;
 
-	if (new_size == 0)
-	{
-		if(ptr == NULL)
-		{
-			p = malloc(old_size);
-			return (p);
-		}
-		free(ptr);
-		return (NULL);
-	}
-	if (ptr == NULL)
+	if (new_size == 0 || ptr == NULL)
 	{
 		p = malloc(new_size);
 	}
