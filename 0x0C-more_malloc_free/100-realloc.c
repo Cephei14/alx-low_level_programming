0ad6e0d023c1301572314b05e430ae1eae4dc124
Cephei14 <rafik.dhaibi@gmail.com>
2023-10-25 06:11:32 -0700

    re

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,7 +18,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	if (ptr == NULL)
+	if (ptr == NULL || ptr == NULL)
 	{
 		p = malloc(new_size);
 	}
