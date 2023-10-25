b42b76c74f7abaa669c1d6335aebc895a615304c
Cephei14 <rafik.dhaibi@gmail.com>
2023-10-25 00:18:37 -0700

    writing a function that splits a string into words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * strtow - function that split a string into two
+ * @str: pointer to the string
+ * Return: pointer or NULL
+ */
+
+char **strtow(char *str)
+{
+	int i = 0, j = 0, k = 0, m, n, wl;
+	char **p;
+
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		if (str[i] == ' ')
+			i++;
+		else
+		{
+			i++, j++;
+			if (str[i] == ' ')
+				k++;
+		}
+	}
+	p = malloc(sizeof(char *) * (j + k + 1));
+	if (p == NULL)
+		return (NULL);
+	i = 0, n = 0;
+	while (i < j)
+	{
+		while (str[i] == ' ')
+			i++, j++;
+		wl = 0;
+		while (str[i + wl] != ' ' && str[i + wl] != '\0')
+			wl++;
+		p[n] = malloc(wl + 1);
+		if (p[n] == NULL)
+			return (NULL);
+		m = 0;
+		while (m < wl)
+		{
+			p[n][m] = str[i + m], m++;
+		}
+		p[n][m] = '\0';
+		n++, i += wl;
+	}
+	p[n] = NULL;
+	return (p);
+}
