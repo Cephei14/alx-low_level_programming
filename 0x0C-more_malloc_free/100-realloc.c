3ed06be412caead8d055b5ed2a29e9e4c61d6dca
Cephei14 <rafik.dhaibi@gmail.com>
2023-10-25 01:19:22 -0700

    ?

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _realloc - function that reallocate a memory
+ * @ptr: old pointer
+ * @old_size: old allocated size with ptr
+ * @new_size: the size that going to be allocated
+ * Return: a pointer to the nes allocated memory
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+}
