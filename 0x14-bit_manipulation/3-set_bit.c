9395cea1af4b6adca88a163f6acbff935ccefe81
Cephei14 <rafik.dhaibi@gmail.com>
2023-10-26 17:36:31 -0700

    add index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,5 +1,4 @@
 #include "main.h"
-unsigned int pwr(unsigned int x, unsigned int i);
 
 /**
  * set_bit - function that set a bit at index
@@ -10,25 +9,8 @@ unsigned int pwr(unsigned int x, unsigned int i);
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	*n = *n + pwr(2, index);
-	return (*n);
-}
-
-/**
- * pwr - function that calculate power of a number
- * @x: number in question
- * @i: x raise to power of i
- * Return: x to power of i
- */
-
-unsigned int pwr(unsigned int x, unsigned int i)
-{
-	unsigned int n = 1, j = 0;
-
-	while (j < i)
-	{
-		n *= x;
-		j++;
-	}
-	return (n);
+	 if (index >= sizeof(*n) * 8)
+		 return (-1);
+	*n = *n | (1 << index);
+		return (1);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 #include <stdio.h>
-unsigned int pwr(unsigned int x, unsigned int i);
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
