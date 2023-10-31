e9eba6a05b57dd83df0cf00ad2d06a54f2a172de
Cephei14 <rafik.dhaibi@gmail.com>
2023-10-30 18:16:45 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	if (p == -1)
 		return (-1);
 	wr = write(p, text_content, l);
-	if (wr == -l)
+	if (wr == -1)
 		return (-1);
 	close(p);
 	return (1);
