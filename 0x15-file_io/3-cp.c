86bc6cc8a824864cf452e578990f81e4ebfa23bf
Cephei14 <rafik.dhaibi@gmail.com>
2023-11-01 21:14:22 -0700

    permission updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,5 @@
 #include "main.h"
-char *c_buff(char *f);
-void c_file(int p);
+void close_it(int fd);
 
 /**
  * main - function that copie contents from file to another file
@@ -11,68 +10,63 @@ void c_file(int p);
 
 int main(int argc, char **argv)
 {
-	int wr, rd, p, to;
-	char *buff;
+	int fd1, rd, fd2, wr;
+	char *buffer;
 
+	buffer = malloc(1024);
+	if (buffer == NULL)
+		return (-1);
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	buff = c_buff(argv[2]);
-	p = open(argv[1], O_RDONLY);
-	rd = read(p, buff, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	do {
-		if (p == -1 || rd == -1)
+	fd1 = open(argv[1], O_RDONLY);
+	rd = read(fd1, buffer, 1024);
+	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd1 == -1 || rd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		free(buffer);
+		exit(98);
+	}
+	while (rd > 0)
+	{
+		if (rd == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			free(buff);
+			free(buffer);
 			exit(98);
 		}
-		wr = write(to, buff, rd);
-		if (to == -1 || wr == -1)
+		wr = write(fd2, buffer, rd);
+		if (fd2 == -1 || wr == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
-			free(buff);
+			free(buffer);
 			exit(98);
 		}
-		rd = read(p, buff, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
-	} while (rd > 0);
-	free(buff);
-	c_file(p);
-	c_file(to);
+		rd = read(fd1, buffer, 1024);
+		fd2 = open(argv[2], O_WRONLY | O_APPEND);
+	}
+	free(buffer);
+	close_it(fd1);
+	close_it(fd2);
 	return (0);
 }
 
 /**
- * c_buff - funtion that create a 1024 buffer
- *@buff: storing file place
- * Return: pointer to the allocated buffer
+ * close_it - function that close a file
+ * @fd: the file in question
  */
 
-char *c_buff(char *buff)
+void close_it(int fd)
 {
-	buff = malloc(1024);
-	if (buff == NULL)
-		return (NULL);
-	return (buff);
-}
+	int c;
 
-/**
- * c_file - function that close file desc
- * @p: file desc in question
- */
-
-void c_file(int p)
-{
-	int t;
-
-	t = close(p);
-	if (t == -1)
+	c = close(fd);
+	if (c == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close file %d\n", p);
+		dprintf(STDERR_FILENO, "Error: Can't close file %d\n", fd);
 		exit(100);
 	}
 }
