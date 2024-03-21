f0b5fa99ddff54870f1b4c16deaeaf0122235165
Cephei14 <rafik.dhaibi@gmail.com>
2024-03-21 07:13:49 -0700

    betty for ex00

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,17 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - print the data in all nodes
+ * @h: head of the linked list
+ * Return: number of nodes
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	unsigned int c;
 
 	c = 0;
-	while(h)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
