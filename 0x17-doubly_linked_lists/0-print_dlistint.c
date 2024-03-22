e67f03ee0cfe0ddc68be0ab2fd7cc949c6800404
Cephei14 <rafik.dhaibi@gmail.com>
2024-03-21 19:59:16 -0700

    test

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,7 +11,7 @@ size_t print_dlistint(const dlistint_t *h)
 	unsigned int c;
 
 	c = 0;
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - function that delete a node at index
+ * @head: of the list
+ * @index: where to delete the list
+ * Return: 1 if success -1 if fail
+ */
+
+
